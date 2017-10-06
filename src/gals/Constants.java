9428d1d5e0ef91b455e16ca256276e3105094b2a
5966868 <5966868@U702624.univali.br>
2017-10-05 21:35:29 -0300

    Inclusão de arquivos

---
+++ src/gals/Constants.java
@@ -0,0 +1,19 @@
+public interface Constants extends ScannerConstants, ParserConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_numero = 2;
+    int t_imprimir = 3;
+    int t_variavel = 4;
+    int t_mais = 5;
+    int t_menos = 6;
+    int t_mult = 7;
+    int t_div = 8;
+    int t_abre = 9;
+    int t_fecha = 10;
+    int t_exponenciacao = 11;
+    int t_recebe = 12;
+    int t_fim = 13;
+
+}
