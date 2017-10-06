7b6f12623257d8578c7b0914e3f84a322b86b96b
5966868 <5966868@U702624.univali.br>
2017-10-05 21:59:58 -0300

    Definição de instruções(switch case)

--- src/gals/Semantico.java
+++ src/gals/Semantico.java
@@ -1,7 +1,82 @@
+
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Semantico implements Constants
 {
+    public Stack<Double> pilha = new Stack<Double>();
+    
+    public Semantico(Stack<Double> pilha) {
+        this.pilha = pilha;
+    }
+    
     public void executeAction(int action, Token token)	throws SemanticError
     {
-        System.out.println("Ação #"+action+", Token: "+token);
+        System.out.println("Aï¿½ï¿½o #"+action+", Token: "+token);
+        String variavel = null;
+        HashMap<String, Double> variaveis = new HashMap<>();
+        boolean querImprimir = false;
+        boolean querAtribuir = false;
+        Double a, b;
+        switch(action){
+            case 0:
+                a = Double.parseDouble(token.getLexeme());
+                if(querAtribuir) {
+                    variaveis.replace(variavel, a);
+                    querAtribuir = false;
+                } else {
+                    pilha.push(a);
+                }                
+            break;
+            case 1:                
+                variavel = token.getLexeme();
+                // verificar se existe
+                variaveis.put(variavel, 0.0);
+                if(querImprimir) {
+                    System.out.println(variavel);
+                    querImprimir = false;
+                }
+                break;
+            case 2:
+                a = pilha.pop();
+                b = pilha.pop();
+                pilha.push(a + b);
+                break;
+            case 3:
+                a = pilha.pop();
+                b = pilha.pop();
+                pilha.push(b - a);
+                break;
+            case 4:
+                a = pilha.pop();
+                b = pilha.pop();
+                pilha.push(a * b);
+                break;
+            case 5:
+                a = pilha.pop();
+                b = pilha.pop();
+                pilha.push(b / a);
+                break;
+            case 6:
+                a = pilha.pop();
+                b = pilha.pop();
+                pilha.push(Math.pow(b, a));
+                break;
+            case 7:
+                querAtribuir = true;
+                break;
+            case 8:
+                // abre
+                break;
+            case 9:
+                // fecha
+                break;
+            case 10:
+                // fim
+                break;
+            case 11:
+                querImprimir = true;
+                break;
+        }
     }	
 }
--- src/marotagelanguage/MarotageLanguage.java
+++ src/marotagelanguage/MarotageLanguage.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package marotagelanguage;
-
+import gals.*;
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author 5966868
@@ -15,7 +18,17 @@ public class MarotageLanguage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Stack<Double> pilha = new Stack<Double>();
+        try {
+            Lexico lex = new Lexico("2 + 9");
+            
+            Sintatico sin = new Sintatico(pilha);
+            Semantico sem = new Semantico();
+            sin.parse(lex, sem);
+            System.out.println("Resultado: " + pilha.pop());
+        } catch (LexicalError ex) {
+            Logger.getLogger(MarotageLanguage.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
